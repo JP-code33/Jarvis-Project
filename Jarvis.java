633508ebce5cccdfb246399d3228246828f96cce
JP-code33 <pateljhenil@gmail.com>
2026-07-17 18:34:54 -0400

    Update Jarvis.java

--- Jarvis.java
+++ Jarvis.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
     boolean isLocked = true; 
     int maxAttempts = 3;
     int attempts = 0;
-    int correctPassword = 4533;
+    int correctPassword = 3309;
 
     while (attempts < maxAttempts) {
         System.out.print("Please enter a numerical password to unlock the system: ");
@@ -34,7 +34,7 @@ public static void main(String[] args) {
     }
 
    if (isLocked) {
-    System.out.println("Access denied! Jarvis is shutting down");
+    System.out.println("\nAccess denied! Jarvis is shutting down");
     System.exit(0);
    } 
    else{
@@ -56,6 +56,49 @@ public static void main(String[] args) {
         else if (command.equalsIgnoreCase("shutdown")) {
             System.out.println("Jarvis: Powering down. Goodbye, Boss.");
             isSystemRunning = false;
+        }
+
+        else if (command.equalsIgnoreCase("remind") || command.equalsIgnoreCase("note")) {
+            System.out.println("Jarvis: Note-taking protocol engaged.");
+            System.out.println("Options [1] Write Note  [2] Read All Notes");
+            System.out.print("Select an option number: ");
+
+            int noteChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            if(noteChoice == 1) {
+                System.out.print("Jarvis: Please enter your note: ");
+                String userNote = scanner.nextLine();
+
+                boolean saved = false;
+                for (int i = 0; i < notesArray.length; i++) {
+                    if(notesArray[i] == null) {
+                        notesArray[i] = userNote;
+                        System.out.println("Jarvis: Note securely logged in." + (i + 1) + ".");
+                        saved = true;
+                        break;
+                    }
+                }
+                if (!saved) {
+                    System.out.println("Warning Memory full.");
+                }
+            }
+            else if (noteChoice == 2) {
+                System.out.println("\n Jarvis System Logs");
+                boolean empty = true;
+                for (int i = 0; i <notesArray.length; i++) {
+                    if (notesArray[i] !=null) {
+                        System.out.println("[" + (i + 1) + "]" + notesArray[i]);
+                        empty = false;
+                    }
+                }
+                if (empty) {
+                    System.out.println("Jarvis: No notes currently found in memory.");
+                }
+            }
+            else {
+                System.out.println("Jarvis: Invalid Selection.");
+            }
         }
 
         else{ 
