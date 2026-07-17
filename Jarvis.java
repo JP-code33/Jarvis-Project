70327f9ee16342d0546f0c96f4ec8779401cc35e
JP-code33 <pateljhenil@gmail.com>
2026-07-17 11:58:18 -0400

    Create Jarvis.java

---
+++ Jarvis.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Jarvis{
+public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+
+    boolean isSystemRunning = true;
+
+    System.out.println("Jarvis: System Activited. Hello, Boss.");
+}
+
+}
